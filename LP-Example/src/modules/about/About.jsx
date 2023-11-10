023d1d188c7aefbec15072dd53ce0f6f6ac06b73
seta1609 <sebastian.stp16@gmail.com>
2023-11-11 00:56:10 +0100

    ft: aboutContent.js its being display now in the component with the context working together

--- LP-Example/src/modules/about/About.jsx
+++ LP-Example/src/modules/about/About.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import "./About.css";
+import {aboutContent} from "./aboutContent.js";
+import {ProfileContext} from "../../context/profile-context.jsx";
 
-function About(){
-return(
-	<div className="About">
-		<h1> Hallo!</h1>
+function About() {
+	const { selectedLanguage } = useContext(ProfileContext);
 
-	</div>
-)
+	const selectedAboutContent = aboutContent.find(
+		(content) => content.language === selectedLanguage
+	);
+
+	if (!selectedAboutContent) {
+		return null;
+	}
+
+	const { greeting, body, quote } = selectedAboutContent.content;
+
+	return (
+		<div className="About">
+			<h1>{greeting}</h1>
+			<p>{body}</p>
+			{quote && (
+				<ul>
+					{quote.map((line, index) => (
+						<li key={index}>{line}</li>
+					))}
+				</ul>
+			)}
+		</div>
+	);
 }
 
 export default About;
--- LP-Example/src/modules/about/aboutContent.js
+++ LP-Example/src/modules/about/aboutContent.js
@@ -1,23 +1,26 @@
- export const aboutContent=[
-     {
-         language:"EN",
-         content:{
-             body:"",
-             quote:""
-         }
-     },
-     {
-         language:"ES",
-         content:{
-             body:"",
-             quote:""
-         }
-     },
-     {
-         language:"DE",
-         content:{
-             body:"",
-             quote:""
-         }
-     }
- ];
+export const aboutContent = [
+    {
+        language: "EN",
+        content: {
+            greeting: "Hi!",
+            body: "Welcome to my Project Page. Here, you will learn a little bit about myself by exploring the page or by directly accessing my social media links. I'm Sebastian Tamayo, a curious developer who strives to spread knowledge in various fields to continuously grow my skills. This growth not only benefits me but also allows me to provide better support to my peers as the complexity of our work increases.",
+            quote: ["It's never too late to learn something new.", "Practice makes the master."]
+        }
+    },
+    {
+        language: "ES",
+        content: {
+            greeting: "¡Hola!",
+            body: "Bienvenido a mi Página de Proyecto. Aquí, podrás conocer un poco más sobre mí explorando la página o accediendo directamente a mis enlaces en redes sociales. Soy Sebastian Tamayo, un desarrollador curioso que busca difundir conocimiento en diversas áreas para seguir mejorando mis habilidades, tanto para mí mismo como para brindar un mejor apoyo a mis colegas cada vez que la complejidad del trabajo aumenta.",
+            quote: ["Nunca es tarde para aprender algo nuevo.", "La práctica hace al maestro."]
+        }
+    },
+    {
+        language: "DE",
+        content: {
+            greeting: "Hallo!",
+            body: "Willkommen auf meiner Projektseite. Hier erfährst du ein wenig über mich, indem du die Seite erkundest oder direkt auf meine Social-Media-Links zugreifst. Ich bin Sebastian Tamayo, ein neugieriger Entwickler, der sein Wissen in vielen Bereichen verbreitet, um seine Fähigkeiten ständig zu erweitern. Dieses Wachstum dient nicht nur mir, sondern ermöglicht es mir auch, meinen Kollegen immer bessere Unterstützung zu bieten, wenn die Komplexität der Arbeit zunimmt.",
+            quote: ["Es ist nie zu spät, etwas Neues zu lernen.", "Übung macht den Meister."]
+        }
+    }
+];
