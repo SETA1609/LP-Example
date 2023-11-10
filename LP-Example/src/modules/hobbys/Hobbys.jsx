4c1fbea8ecbcc41b1465a9f7431e657e6166a3ab
seta1609 <sebastian.stp16@gmail.com>
2023-11-10 10:46:13 +0100

    ft: cards where declared and content object was written

---
+++ LP-Example/src/modules/hobbys/Hobbys.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import'./Hobbys.css';
+import content from './content';
+import Card from './card/Card';
+
+function Hobbys(content){
+
+
+	return(
+		<div>
+		< Card />
+		</div>
+	)
+}
+
+export default Hobbys;
